62536a6aa1f634a1d758e01b98928dcd3569c9b1
katiecoburn <katie.m.coburn@gmail.com>
2026-04-07 14:31:53 -0700

    updated lecture examples

--- Lecture Examples/lecture2_api_examples/cat_pictures_api/catapp.py
+++ Lecture Examples/lecture2_api_examples/cat_pictures_api/catapp.py
@@ -7,7 +7,7 @@ from typing import Optional
 app = FastAPI()
 
 # Directory where images are stored
-cat_pic_dir = "images"
+cat_pic_dir = "cat_pictures_api/images"
 
 # Make sure that directory exists (if not, make it)
 if not os.path.exists(cat_pic_dir):
@@ -21,6 +21,8 @@ def read_root():
 def get_random_image(cat_name: Optional[str] = None):
   # List all image files in the directory
   image_files = [f for f in os.listdir(cat_pic_dir) if os.path.isfile(os.path.join(cat_pic_dir, f))]
+
+  display_name = "Our Cats"
   
   # Filter by cat name if specified
   if cat_name:
@@ -29,6 +31,11 @@ def get_random_image(cat_name: Optional[str] = None):
       raise HTTPException(status_code=400, detail="Cat name must be Gal or Oppie")
     
     image_files = [f for f in image_files if cat_name in f.lower()]
+
+    if cat_name == "gal":
+       display_name = "Galavant"
+    elif cat_name == "oppie":
+       display_name = "Opal"
     
   # Check if there are any images; if not, give an error
   if not image_files:
@@ -36,16 +43,16 @@ def get_random_image(cat_name: Optional[str] = None):
   
   # Select a random image
   random_image = random.choice(image_files)
-  image_path = os.path.join(cat_pic_dir, random_image)
+  image_url = f"/images/{random_image}"
   
   html_content = f"""
     <html>
         <head>
-            <title>Random Picture of {cat_name}</title>
+            <title>Random Picture of {display_name}</title>
         </head>
         <body>
-            <h1>Random picture of {cat_name}</h1>
-            <img src="{image_path}" alt="Random Image" style="max-width: 30%; height: auto;">
+            <h1>Random picture of {display_name}</h1>
+            <img src="{image_url}" alt="Random Image" style="max-width: 30%; height: auto;">
         </body>
     </html>
     """
@@ -58,6 +65,6 @@ if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="127.0.0.1", port=8000)
 
-# source this file with python app.py in terminal,
-# then go to http://localhost:8000/api/greet or api/test!
+# source this file with python cat_pictures_api/app.py in terminal,
+# then go to http://127.0.0.1:8000/ or http://127.0.0.1:8000/random
 # documentation is automatically generated at http://localhost:8000/docs
